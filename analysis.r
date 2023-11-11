84850a92efab8e710a2257573a9ded2894ed77b7
Matthew Garber <mgarber441@gmail.com>
2023-11-11 17:29:18 -0500

    Updating example to include two data-sources

--- analysis.r
+++ analysis.r
@@ -1,19 +1,35 @@
 # Load The .RDA Data into your working environment
 # Replace the Path with where your data-set is located.
 load("C:\\Users\\MRAMERICA-PC\\Desktop\\36158-0001-Data.rda")
+load("C:\\Users\\MRAMERICA-PC\\Desktop\\36158-0002-Data.rda")
+
+# Declare Data Objects
+data1 <- da36158.0001
+data2 <- da36158.0002
+
+# Combine DataSet
+# Select only relevant columns
+# as the two data-sources aren't exactly 1:1
+data1_selected <- data1[c("SCWM_JSAT", "SCEM_DIST")]
+data2_selected <- data2[c("SCWM_JSAT", "SCEM_DIST")]
+
+# Create an object that represents the combined data
+combined_data <- rbind(data1_selected,data2_selected)
 
 # Create Correlation between Job Satisfation and Psych Distress
-cor_coef <- cor(data$SCWM_JSAT, data$SCEM_DIST, use = "complete.obs")
+cor_coef <- cor(combined_data$SCWM_JSAT, combined_data$SCEM_DIST, use = "complete.obs")
 
-# Install GGplot2
+# Install and use ggplot 2 Library
 install.packages("ggplot2")
 library(ggplot2)
 
 # Create the plot
-ggplot(data, aes(x = SCWM_JSAT, y = SCEM_DIST)) +
+ggplot(da36158.0001, aes(x = SCWM_JSAT, y = SCEM_DIST)) +
   geom_point() + # Scatter plot
   geom_smooth(method = lm, color = "blue") + # Regression line
   ggtitle("Correlation between Job Satisfaction and Psychological Distress") +
   xlab("Job Satisfaction") +
   ylab("Psychological Distress") +
-  annotate("text", x = min(data$SCWM_JSAT), y = max(data$SCEM_DIST), label = paste("r = ", round(cor_coef, 2)), hjust = 0, vjust = 1)
+  annotate("text", x = min(combined_data$SCWM_JSAT), y = max(combined_data$SCEM_DIST), label = paste("r = ", round(cor_coef, 2)), hjust = 0, vjust = 1)
+
+
